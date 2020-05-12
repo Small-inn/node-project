b9b658cc082acd438818ded5482f040dff41aa73
liuxie <small_inn@163.com>
2020-05-13 00:37:47 +0800

    错误拼写

--- src/helper/range.js
+++ src/helper/range.js
@@ -1,12 +1,12 @@
 module.exports = (totalSize, req, res) => {
   const range = req.headers['range'];
   if (!range) {
-    code: 200;
+    200;
   }
 
   const sizes = range.match(/bytes=(\d*)-(\d*)/);
   const end = sizes[2] || totalSize - 1;
-  const start = size[1] || totalSize - end;
+  const start = sizes[1] || totalSize - end;
 
   if (start > end || start < 0 || end > totalSize) {
       return { code: 200 };
