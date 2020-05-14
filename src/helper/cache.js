acbb64283f84ecd1227fccca88bca5a523ea68b5
Small-inn <small_inn@163.com>
2020-05-14 16:59:01 +0800

    cli准备

---
+++ src/helper/cache.js
@@ -0,0 +1,38 @@
+const { cache } = require('../config/index');
+
+function refreshRes(stats, res) {
+  const { maxAge, expires, cacheControl, lastModified, etag } = cache
+
+  if (expires) {
+    res.setHeader('Expires', new Date(Date.now() + maxAge * 1000).toUTCString())
+  }
+
+  if (cacheControl) {
+    res.setHeader('Cache-Control', `public, max-age=${maxAge}`)
+  }
+
+  if (lastModified) {
+    res.setHeader('Last-Modifide', stats.mtime.toUTCString())
+  }
+
+  if (etag) {
+    res.setHeader('Etag', `${stats.size}-${stats.mtime}`)
+  }
+}
+
+module.exports = function isFresh(stats, req, res) {
+  refreshRes(stats, res)
+  const lastModified = req.headers['if-modified-since']
+  const etag = req.headers['if-none-match']
+
+  if (!lastModified && !etag) {
+    return false
+  }
+
+  if (lastModified && lastModified !== res.getHeader('Last-Modifide')) {
+    return false
+  }
+  if (etag && etag !== res.getHeader('Etag')) {
+    return false
+  }
+}
--- src/helper/route.js
+++ src/helper/route.js
@@ -12,19 +12,28 @@ const template = Handlebars.compile(source.toString());
 const mime = require('./mime');
 const compress = require('./compress');
 const range = require('./range');
+const isFresh = require('./cache');
 
 module.exports = async function (req, res, filePath) {
   try {
     const stats = await stat(filePath)
     if (stats.isFile()) {
       const contentType = mime(filePath);
-      res.statusCode = 200;
       res.setHeader('Content-Type', contentType);
+
+      if (isFresh(stats, req, res)) {
+        res.statusCode = 304;
+        res.end();
+        return
+      }
+
       let rs;
       const { code, start, end } = range(stats.size, req, res)
       if (code === 200) {
+        res.statusCode = 200;
         rs = fs.createReadStream(filePath)
       } else {
+        res.statusCode = 206;
         rs = fs.createReadStream(filePath, {start, end})
       }
       // fs.createReadStream(filePath).pipe(res);
